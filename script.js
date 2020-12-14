0cffdefa3a5410d437986170a6f4c7426782e53e
Bogdan <iamnfs@hotmail.com>
2020-12-14 12:31:22 -0700

    updates

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 // Initialize empty library
 let myLibrary = [];
 
+// Loop through library to display elements by row
+function libraryLoop (myLibrary) {
+  data = document.getElementById("table-data");
+  while (data.hasChildNodes()) {
+    data.removeChild(data.lastChild);
+  }
+  for (let i = 0; i < myLibrary.length; i++) {
+    let table = document.getElementById("table-data");
+    let row = table.insertRow();
+    row.id = "row" + (i+1);
+    let cell0 = row.insertCell(0);
+    let cell1 = row.insertCell(1);
+    let cell2 = row.insertCell(2);
+    let cell3 = row.insertCell(3);
+    let cell4 = row.insertCell(4);
+    cell0.innerHTML = myLibrary[i].title;
+    cell1.innerHTML = myLibrary[i].author;
+    cell2.innerHTML = myLibrary[i].pages;
+    cell3.innerHTML = myLibrary[i].read == true? "Yes":"No";
+    cell0.className = "cell";
+    cell1.className = "cell";
+    cell2.className = "cell";
+    cell3.className = "cell";
+    let button = document.createElement("button");
+    button.innerHTML = "X";
+    button.className = "delete-btn";
+    button.id = String(i+1);
+    button.setAttribute("onclick",`deleteRow(${i})`);
+    cell4.appendChild(button);
+  }
+}
 
 // Book object constructor function
 function Book(title,author,pages,read) {
@@ -19,8 +50,10 @@ function addBookToLibrary() {
     let title = document.getElementById("title").value;
     let author = document.getElementById("author").value;
     let pages = document.getElementById("pages").value;
-    let read = document.querySelector('input[name="read"]:checked').value == "yes"? true:false ;
+    let read = document.querySelector('input[name="read"]:checked').value == "yes"? true:false;
+    book = new Book(title,author,pages,read);
     myLibrary.push(book);
+    libraryLoop(myLibrary);
     closeForm();
     return false;
 }
@@ -43,4 +76,10 @@ function clearForm() {
   document.getElementById("pages").value = null;
   document.getElementById("yes").checked = false;
   document.getElementById("no").checked = false;
+}
+
+// Delete item from array and rerun libraryLoop to display updated array
+function deleteRow(i) {
+  myLibrary.splice(i,1);
+  libraryLoop (myLibrary);
 }
