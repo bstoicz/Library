346a7f3bbb62e838ae4574b98612f0d3b03f3ed7
Bogdan <iamnfs@hotmail.com>
2020-12-14 09:53:37 -0700

    updates

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Initialize empty library
 let myLibrary = [];
 
+
 // Book object constructor function
 function Book(title,author,pages,read) {
   this.title = title;
@@ -9,22 +10,37 @@ function Book(title,author,pages,read) {
   this.read = read;
 }
 
-book1 = new Book("bam","bog",200,true);
-book2 = new Book("com","gog",100,true);
+// Event listener for form submittal
+document.getElementById('add-new').onsubmit = addBookToLibrary;
 
-// Add new books to library
-function addBookToLibrary(book) {
-  myLibrary.push(book);
+
+// Add book object to library from form
+function addBookToLibrary() {
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let read = document.querySelector('input[name="read"]:checked').value == "yes"? true:false ;
+    myLibrary.push(book);
+    closeForm();
+    return false;
 }
 
-function addToLibrary() {
-    console.log("k")
-}
-
+// Open popup form function
 function openForm() {
     document.getElementById("form").style.display = "block";
   }
-  
-  function closeForm() {
+
+// Close popup form function
+function closeForm() {
     document.getElementById("form").style.display = "none";
+    clearForm();
   }
+
+// Clear form of all input information 
+function clearForm() {
+  document.getElementById("title").value = null;
+  document.getElementById("author").value = null;
+  document.getElementById("pages").value = null;
+  document.getElementById("yes").checked = false;
+  document.getElementById("no").checked = false;
+}
